b5280c9fae8b31334cbd71d226b9d92be2ccb5ad
Manolis-Tasiopoulos <manolakis1723@gmail.com>
2021-10-01 10:57:00 +0300

    Adding telegramBOT.py and Transactions.csv
    * telegramBOT.py purpose:
       - This file has the code in order the bot to start running
       - The main goal of this bot is to monitoring the Bitcoin address of the Whale
       - Every 20 seconds BOT will run scanner to check for new transactions
       - If a scanner.py catch a new transaction the bot wil send a message with the details of it
       - Bot have two basics commands. One for starting (\start) and one for stop (\stop) monitoring
    
    * Transactions.csv purpose:
       - Just the CSV file that contains all the transactions that whale made

---
+++ telegramBOT.py
@@ -0,0 +1,46 @@
+import telegram.ext
+import scanner
+
+
+def check_address(context):
+    missing_transactions = scanner.main(verbose=False)
+    str_tx = ''
+
+    if missing_transactions is None:
+        missing_transactions = []
+
+    # print(missing_transactions)
+    if len(missing_transactions) != 0:
+        str_tx = '---NEW TRANSACTION---\n\n'
+        for tx in missing_transactions:
+            for key, value in tx.items():
+                # print('\t', key, ' : ', value)
+                str_tx += key + ': ' + str(value)
+
+                if key != 'total cost ($)':
+                    str_tx += '\n'
+
+    if str_tx != '':
+        context.bot.send_message(chat_id=context.job.context, text=str_tx)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='Start monitoring...')
+    context.job_queue.run_repeating(check_address, 20, context=update.message.chat_id)
+
+
+def stop(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='Stopped!')
+    context.job_queue.stop()
+
+
+with open('token.txt', 'r') as f:
+    TOKEN = f.read()
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+
+updater.dispatcher.add_handler(telegram.ext.CommandHandler('start', start))
+updater.dispatcher.add_handler(telegram.ext.CommandHandler('stop', stop))
+updater.start_polling()
+updater.idle()
